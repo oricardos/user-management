e341db91cc44a15aadca03edd2c06e86dba7a5e4
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-10-01 00:42:05 -0300

    add photo file

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,9 +10,37 @@ class UserController {
 
         this.formEl.addEventListener("submit", event => {
             event.preventDefault();
+
+            let values = this.getValues();
+
+            values.photo = "";
+
+            this.getPhoto((content) => {
+                values.photo = content;
+
+
+                this.addLine(values);
+            });
             
-            this.addLine(this.getValues());
         });
+    }
+
+    getPhoto(callback){
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+            if (item.name === 'photo') {
+                return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            callback(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     getValues(){
@@ -43,7 +71,7 @@ class UserController {
     addLine(dataUser){
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
